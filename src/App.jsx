e7a1cbb3510175582853af7a4c0fa84affe3ad3c
itsmechaithanya <itsmechaithanyam@gmail.com>
2024-07-04 20:45:26 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Card from "./Components/Card";
 // import Card from "./Components/Card";
 
 function App (){
@@ -6,13 +7,10 @@ function App (){
 	const names = ["Chaithanya","Shravya","rajini"];
 
 	return(
-	<div>
-		{/* {names.map((elem,index)=>(<div className="w-fit px-3 py-4 m-2 bg-zinc-200 rounded-xl">
-			{elem}
-		</div>))} */}
-		{names.map((elem,index)=>(<div className="w-fit px-3 py-4 m-2 bg-zinc-200 rounded-xl">{elem}</div>))}
-		
-	</div>
+	<>
+		<Card/>
+		<h1>hey</h1>
+	</>
 	)
 }
 export default App
--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,17 +1,24 @@
 import React from 'react'
 
 function Card() {
+
+    const data = [
+        {img: 'https://images.unsplash.com/photo-1701685493726-7ecbbbd0b199?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDN8fHxlbnwwfHx8fHw%3D', title:"amazon box", dis:"Lorem ipsum dolor sit, amet consectetur adipisicing elit."},
+        {img: 'https://images.unsplash.com/photo-1719822077838-b48bf2c7903d?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90b3MtZmVlZHw3fHx8ZW58MHx8fHx8', title:"something", dis:"Lorem ipsum dolor sit."},
+        {img: 'https://images.unsplash.com/photo-1719831400714-5d4fe425cd8a?q=80&w=3087&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D ̑', title:"somethings", dis:"lorem19"}
+    ]
   return (
-    <div className='w-full h-screen bg-zinc-200'>
-        <div className='w-52 bg-zinc-50 absolute left-1/2 top-1/2 -translate-x-[50%] -translate-y-[50%] rounded-md overflow-hidden'>
+    <div className='w-full h-screen bg-zinc-200 flex items-center justify-center gap-10'>
+        {data.map((elem,index)=>(<div key={index} className='w-52 bg-zinc-50 rounded-xl overflow-hidden'>
             <div className='h-42 w-full bg-zinc-300'>
-                <img className='w-full h-full object-cover' src="https://images.unsplash.com/photo-1522780550166-284a0288c8df?q=80&w=2592&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+                <img className='w-full h-full object-cover' src={elem.img} alt="" />
             </div>
             <div className='w-full px-3 py-4'>
-                <h2 className='font-semibold'>Amazon Basics.</h2>
-                <p className='text-xs mt-3'>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+                <h2 className='font-semibold'>{elem.title}</h2>
+                <p className='text-xs mt-3'>{elem.dis}</p>
             </div>
-        </div>
+        </div>))}
+        
     </div>
   )
 }
