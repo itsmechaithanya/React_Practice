5a059e0531284c7071fbc49c363a253b487c676c
itsmechaithanya <itsmechaithanyam@gmail.com>
2024-07-28 15:58:33 +0530

    first commit]

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,15 @@ import Card from "./Components/Card";
 import Cards from "./Components/Handling";
 import Handling from "./Components/Handling";
 import Use_State_basic from "./Components/Use_State_basic";
+import Use_state_int from "./Components/Use_state_int";
 
 function App (){
 	const names = ["Chaithanya","rajini"];
 
 	return(
 	<>
-		<Use_State_basic/>
+		<Use_state_int/>
+		{/* <Use_State_basic/> */}
 		{/* <Handling/> */}
 		{/* <Card/> */}
 		{/* <h1>hey</h1> */} 
--- src/Components/Use_state_int.jsx
+++ src/Components/Use_state_int.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function Use_state_int() {
+  var [a,b] = useState(12)
   return (
-    <div>
-        Use_state_int
+    <div className='p-2'>
+        <h1>{a}</h1>
+        <button className='bg-zinc-400 px-3 py-1 rounded-xl'>change</button>
     </div>
   )
 }
