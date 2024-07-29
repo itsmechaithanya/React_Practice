aea9704d18165ab02baf4a2b284404b99d032350
itsmechaithanya <itsmechaithanyam@gmail.com>
2024-07-29 12:31:14 +0530

    first commit]

--- src/Components/Mastering_useStatetwo.jsx
+++ src/Components/Mastering_useStatetwo.jsx
@@ -4,7 +4,10 @@ function Mastering_useStatetwo() {
     var [val,setVal] = useState({name:"chey", age:21})
   return (
     <div>
-
+        <button onClick={()=>{
+            setVal({...val, gender: "male"});
+            console.log(val)
+        }}>Print</button>
     </div>
   )
 }
